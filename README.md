894f9a493d503e063b7e6b4583aa9aa1f759e904
Arley McBlain <myfriendarley@arleym.com>
2023-06-15 09:44:07 -0400

    init commit with Bootstrap and Webpack

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Cardhouse-2023
+
+# Cardhouse 2023
+
 For quick 'n' dirty static design-in-browser with HTML5 Boilerplate, Bootstrap 5.3, Webpack
+
+## About
+- Bootstrap 5.3 [Read more](https://getbootstrap.com/docs/5.3/getting-started/webpack)
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// 1. Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+// 2. or just load plugins individually
+// import Alert from 'bootstrap/js/dist/alert'
+// 3. or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap'
